550fc2d59a60842e00bca341bea3904d7ef1f720
Mattias Hamberg <mattias@kretiochpleti.se>
2016-09-01 19:37:39 +0300

    Bug fix

--- updates/change_employees_table_20160831.php
+++ updates/change_employees_table_20160831.php
@@ -3,20 +3,20 @@
 use October\Rain\Database\Updates\Migration;
 use Schema;
 
-class ChangeEmployeesTable20160816 extends Migration
+class ChangeEmployeesTable20160831 extends Migration
 {
 
 	public function up()
 	{
 		Schema::table('hambern_company_employees', function ($table) {
-			$table->text('social_media')->nullable();
+			$table->text('social_media')->nullable()->change();
 		});
 	}
 
 	public function down()
 	{
-		Schema::table('hambern_company_employees', function($table) {
-				$table->dropColumn('social_media');
+		Schema::table('hambern_company_employees', function ($table) {
+			$table->text('social_media')->change();
 		});
 	}
 }
