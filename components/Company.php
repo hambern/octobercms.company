e8849e721211d485103b491ffaaeb2afd3efc775
Anne Datema <info@annedatema.nl>
2017-07-28 20:53:28 +0200

    PSR2

--- components/Company.php
+++ components/Company.php
@@ -8,46 +8,46 @@ use October\Rain\Database\Model;
 class Company extends ComponentBase
 {
 
-	public function componentDetails()
-	{
-		return [
-			'name'        => 'hambern.company::lang.components.company.name',
-			'description' => 'hambern.company::lang.components.company.description',
-		];
-	}
+    public function componentDetails()
+    {
+        return [
+            'name' => 'hambern.company::lang.components.company.name',
+            'description' => 'hambern.company::lang.components.company.description',
+        ];
+    }
 
-	public function defineProperties()
-	{
-		return [];
-	}
+    public function defineProperties()
+    {
+        return [];
+    }
 
-	public function onRun()
-	{
-		$settings = Settings::instance();
-		$company = new Model();
-		$company->name = $settings->name;
-		$company->slogan = $settings->slogan;
-		$company->logo = $settings->logo;
-		$company->story = $settings->story;
-		$company->phone = $settings->phone;
-		$company->fax = $settings->fax;
-		$company->email = $settings->email;
-		$company->address = $settings->address;
-		$company->social_media = $settings->social_media;
-		$company->contact = $this->contact();
-		$company->street_name = $settings->street_name;
-		$company->street_number = $settings->street_number;
-		$company->zip = $settings->zip;
-		$company->city = $settings->city;
-		$this->page['company'] = $company;
-	}
+    public function onRun()
+    {
+        $settings = Settings::instance();
+        $company = new Model();
+        $company->name = $settings->name;
+        $company->slogan = $settings->slogan;
+        $company->logo = $settings->logo;
+        $company->story = $settings->story;
+        $company->phone = $settings->phone;
+        $company->fax = $settings->fax;
+        $company->email = $settings->email;
+        $company->address = $settings->address;
+        $company->social_media = $settings->social_media;
+        $company->contact = $this->contact();
+        $company->street_name = $settings->street_name;
+        $company->street_number = $settings->street_number;
+        $company->zip = $settings->zip;
+        $company->city = $settings->city;
+        $this->page['company'] = $company;
+    }
 
-	public function contact()
-	{
-		if ($employee = Employee::find(Settings::get('contact'))) {
-			return $employee;
-		}
-		return Employee::orderBy('id', 'asc')->first();
-	}
+    public function contact()
+    {
+        if ($employee = Employee::find(Settings::get('contact'))) {
+            return $employee;
+        }
+        return Employee::orderBy('id', 'asc')->first();
+    }
 
 }
